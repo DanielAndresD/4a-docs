366fae88bcedd0b5bab6f52cdd98b6882828223b
Neixrinf <83479529+neixrinf@users.noreply.github.com>
2021-11-20 09:29:30 -0500

    actualizacion def get

--- userMS/authApp/views/usuariosDetailView.py
+++ userMS/authApp/views/usuariosDetailView.py
@@ -2,9 +2,11 @@ from rest_framework import generics
 from authApp.models.usuarios import usuarios
 from authApp.serializers.usuarios_serializer import usuarios_serializer
 
-
-
-
 class UsuariosDetailView(generics.RetrieveAPIView):
     queryset = usuarios.objects.all()
     serializer_class = usuarios_serializer
+    
+    def get (self, requerest):
+        lista_usuarios = usuarios.objects.all()
+        serializers = usuarios_serializer(lista_usuarios, many=True)
+        return Response(serializers.data, 200)
